afbb9bf0dc99a0cf9baf4dd48e5e26b2362213b2
Mokshith <Mokshith@LAPTOP-E5D4RPVB>
2024-08-07 12:15:46 +0530

    my first commit

---
+++ MVN/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,20 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+
+	WebDriver driver=new ChromeDriver();
+	@Test
+	
+	public void createCustomer()
+	{
+	
+	Reporter.log("Hai",true);
+
+	}
+
+}
